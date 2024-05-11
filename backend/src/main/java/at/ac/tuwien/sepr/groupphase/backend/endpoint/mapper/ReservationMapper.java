c8d329646ab450423cfa72759bc575506153d865
erker <e12229254@student.tuwien.ac.at>
2024-05-11 16:50:48 +0200

    3.2A.1 Removed Mapper because of bugs

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/ReservationMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/ReservationMapper.java
@@ -1,18 +1,15 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper;
 
-import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.ReservationListDto;
-import at.ac.tuwien.sepr.groupphase.backend.entity.Reservation;
-import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Named;
-
-import java.util.List;
 
 @Mapper
 public interface ReservationMapper {
+    /*
     @Named("reservationList")
     ReservationListDto reservationToReservationListDto(Reservation reservation);
 
     @IterableMapping(qualifiedByName = "reservationList")
     List<ReservationListDto> reservationToReservationListDto(List<Reservation> reservation);
+
+     */
 }
