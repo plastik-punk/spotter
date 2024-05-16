82a718c99825cf08446fc279837bb5a5180e8d73
Felix Weilharter <e11908535@student.tuwien.ac.at>
2024-05-16 18:36:05 +0200

    Add Account cretion for Employee accounts as admin

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import {LoginComponent} from './components/login/login.component';
 import {AuthGuard} from './guards/auth.guard';
 import {MessageComponent} from './components/message/message.component';
 import {RegistrationComponent} from "./components/registration/registration.component";
+import {UserManagementComponent} from "./components/user-managment/user-managment.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'home', component: HomeComponent},
   {path: 'message', canActivate: mapToCanActivate([AuthGuard]), component: MessageComponent},
-  {path: 'registration', component:RegistrationComponent}
+  {path: 'registration', component:RegistrationComponent},
+  {path: 'employees', component:UserManagementComponent}
 ];
 
 @NgModule({
