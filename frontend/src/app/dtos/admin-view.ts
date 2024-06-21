a8c12d46dfcee4079a283ad0451c45b0c05b6af7
Leon Stempfer <e12222127@student.tuwien.ac.at>
2024-06-21 20:24:16 +0200

    [LeSt] reformatted

--- frontend/src/app/dtos/admin-view.ts
+++ frontend/src/app/dtos/admin-view.ts
@@ -1,19 +1,22 @@
-export interface AdminViewDto{
+export interface AdminViewDto {
   areaId: number,
   startTime: Date,
   date: Date
 }
-export interface PredictionDto{
+
+export interface PredictionDto {
   predictionText: string,
   areaNames: Array<string>,
   predictions: Array<number>
 }
-export interface ReservationForeCastDto{
+
+export interface ReservationForeCastDto {
   forecast: Array<number>,
   maxPlace: number,
   days: Array<string>
 }
-export interface UnusualReservationsDto{
+
+export interface UnusualReservationsDto {
   days: Array<string>,
   messages: Array<string>,
   unusual: boolean
