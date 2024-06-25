3af7f232edbfd558f6c9546916f310d1deba027c
Jan König <e1007167@student.tuwien.ac.at>
2024-06-25 13:54:17 +0200

    [JaKö] Logging in layout endpoint

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/LayoutEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/LayoutEndpoint.java
@@ -1,6 +1,5 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint;
 
-
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.AreaDetailDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.AreaDetailListDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.AreaLayoutDto;
@@ -27,7 +26,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import java.lang.invoke.MethodHandles;
 
 @RestController
@@ -41,13 +39,12 @@ public class LayoutEndpoint {
         this.layoutService = layoutService;
     }
 
-
     @ResponseStatus(HttpStatus.OK)
     @PermitAll
     @GetMapping
     @Operation(summary = "Get layout of area for requested time and pax")
     public AreaLayoutDto getAvailabilityLayout(@Valid ReservationLayoutCheckAvailabilityDto reservationLayoutCheckAvailabilityDto) {
-        LOGGER.info("GET /api/v1/reservations/layout body: {}", reservationLayoutCheckAvailabilityDto);
+        LOGGER.info("GET /api/v1/layout body: {}", reservationLayoutCheckAvailabilityDto);
         return layoutService.getAreaLayout(reservationLayoutCheckAvailabilityDto);
     }
 
@@ -56,7 +53,7 @@ public class LayoutEndpoint {
     @GetMapping("/areas")
     @Operation(summary = "Get list of all areas")
     public AreaListDto getAllAreas() {
-        LOGGER.info("GET /api/v1/reservations/areas");
+        LOGGER.info("GET /api/v1/layout/areas");
         return layoutService.getAllAreas();
     }
 
@@ -65,61 +62,61 @@ public class LayoutEndpoint {
     @GetMapping("/areas/detailed")
     @Operation(summary = "Get list of all areas with details")
     public AreaDetailListDto getAllAreasDetailed() {
-        LOGGER.info("GET /api/v1/reservations/areas/detailed");
+        LOGGER.info("GET /api/v1/layout/areas/detailed");
         return layoutService.getAllAreasDetailed();
     }
 
-    // TODO: logging
     @ResponseStatus(HttpStatus.CREATED)
     @PermitAll
     @PostMapping("/create")
     @Operation(summary = "Create layout")
     public void createLayout(@RequestBody @Valid LayoutCreateDto layoutCreateDto) {
+        LOGGER.info("POST /api/v1/layout body: {}", layoutCreateDto.toString());
         layoutService.createLayout(layoutCreateDto);
     }
 
-    // TODO: logging
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @Secured({"ROLE_ADMIN"})
     @DeleteMapping("/delete/{id}")
     @Operation(summary = "Delete area")
     public void deleteArea(@PathVariable("id") Long id) throws ConflictException {
+        LOGGER.info("DELETE /api/v1/layout/{}", id);
         layoutService.deleteArea(id);
     }
 
-    // TODO: logging
     @ResponseStatus(HttpStatus.OK)
     @PermitAll
     @GetMapping("/area/{id}")
     @Operation(summary = "Get layout of area")
     public AreaDetailDto getAreaById(@PathVariable("id") Long id) {
+        LOGGER.info("GET /api/v1/layout/area/{}", id);
         return layoutService.getAreaById(id);
     }
 
-    // TODO: logging
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @Secured({"ROLE_ADMIN", "ROLE_EMPLOYEE"})
     @PutMapping("/toggleOpen/{id}")
     @Operation(summary = "Toggle open status of area")
     public void toggleOpen(@PathVariable("id") Long id, @RequestBody Boolean isOpen) {
+        LOGGER.info("PUT /api/v1/layout/toggleOpen/{} body: {}", id, isOpen);
         layoutService.toggleOpen(id, isOpen);
     }
 
-    // TODO: logging
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @Secured({"ROLE_ADMIN"})
     @PutMapping("/area/update")
     @Operation(summary = "Update area")
     public void updateArea(@RequestBody AreaDetailDto areaDetailDto) throws ConflictException {
+        LOGGER.info("PUT /api/v1/layout/area/update body: {}", areaDetailDto.toString());
         layoutService.updateArea(areaDetailDto);
     }
 
-    // TODO: logging
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @Secured({"ROLE_ADMIN"})
     @PutMapping("/toggleMain/{id}")
     @Operation(summary = "Toggle main status of area")
     public void toggleMain(@PathVariable("id") Long id, @RequestBody Boolean isMain) {
+        LOGGER.info("PUT /api/v1/layout/toggleMain/{} body: {}", id, isMain);
         layoutService.toggleMain(id, isMain);
     }
 }
